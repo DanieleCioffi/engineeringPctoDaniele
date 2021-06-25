212c6f929fec01003bad52ae56c5e226270f3d04
DanieleCioffi <85867609+DanieleCioffi@users.noreply.github.com>
2021-06-25 17:07:26 +0200

    fix: modifica pagina trailer e description

--- src/views/components/details/Details.js
+++ src/views/components/details/Details.js
@@ -4,6 +4,8 @@ class Details {
   render() {
     this.location = location.pathname;
     this.movieId = location.pathname.split("/")[2];
+    this.data;
+    this.trailer;
     console.log(this.movieId);
 
     fetch(
@@ -11,69 +13,87 @@ class Details {
     )
       .then((response) => response.json())
       .then((data) => {
-        document.getElementById("main-container").innerHTML = `
-            <div class ="center-div" id="topDiv">
-                <div class="card mb-3" id="details">
-                    <div class="row no-gutters">
-                        <div class="col-md-3">
-                            <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${data.poster_path}" class="img-fluid" alt="Responsive image">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body" id="cardBody">
-                                <div>
-                                    <h5 class="card-title" id="detailsTitle"></h5>
-                                    <p class="card-text" id="detailsText"></p>
-                                </div>
-                                <div class="btn-div">
-                                    <button class="btn btn-primary" id="backBtn">Back</button>
-                                    <button class="btn btn-primary"id="trailerBtn">Trailer</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-
-        document.getElementById("detailsTitle").innerHTML = data.title;
-        document.getElementById("detailsText").innerHTML = data.overview;
-        this.addActionListener();
+        this.data = data;
+        this.showDescription();
       });
   }
 
-  addActionListener() {
+  addBackListener() {
     document.getElementById("backBtn").addEventListener("click", () => {
+      this.trailer = undefined;
       window.history.back();
     });
+  }
 
+  addTrailerListener() {
     document.getElementById("trailerBtn").addEventListener("click", () => {
-      history.pushState({ page: 3 }, "title 3", location.pathname + "/trailer");
       this.showTrailer();
     });
   }
 
-  showTrailer() {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${this.movieId}/videos?api_key=c4d79d0d1e50bf8bc86b7afbd240e4df&language=en-US`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        document.getElementById("main-container").innerHTML = `
-            <div class="videoDiv">
-                <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/${data.results[0].key}" allowfullscreen></iframe>
+  addDescriptionListener() {
+    document.getElementById("descriptionBtn").addEventListener("click", () => {
+      this.showDescription();
+    });
+  }
+
+  showDescription() {
+    document.getElementById("main-container").innerHTML = `
+            <div class="center">
+            <div class="card mb-3" style="max-width:50%">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                <img src=https://www.themoviedb.org/t/p/w600_and_h900_bestv2${this.data.poster_path} class="img-fluid" alt="...">
                 </div>
-                <div class="btn-div">
-                    <button class="btn btn-primary" id="backBtn">Back</button>
+                <div class="col-md-8">
+                <div class="card-body" id="cardBody">
+                    <h5 class="card-title" style="font-size: 2rem">${this.data.title}</h5>
+                    <p class="card-text" id="descriptionContainer" style="font-size: 1rem">${this.data.overview}</p>
+                    <div class="btn-div">
+                        <button id="backBtn" class="btn btn-primary" >Back</button>
+                        <button id="trailerBtn" style="float:right" class="btn btn-primary">Trailer</button>
+                    </div>
                 </div>
                 </div>
             </div>
-        `;
+            </div>
+        </div>
+    `;
 
-        document.getElementById("backBtn").addEventListener("click", () => {
-          window.history.back();
+    this.addBackListener();
+    this.addTrailerListener();
+  }
+
+  async showTrailer() {
+    if (this.trailer === undefined) {
+      await fetch(
+        `https://api.themoviedb.org/3/movie/${this.movieId}/videos?api_key=c4d79d0d1e50bf8bc86b7afbd240e4df&language=en-US`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          this.trailer = data;
         });
-      });
+    }
+
+    document.getElementById("cardBody").innerHTML = `
+        <h5 class="card-title" style="font-size: 2rem">${this.data.title}</h5>
+        <div class="embed-responsive embed-responsive-4by3">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/${this.trailer.results[0].key}" allowfullscreen></iframe>
+        </div>
+        <div class="btn-div">
+            <button class="btn btn-primary" id="backBtn">Back</button>
+            <button class="btn btn-primary" id="descriptionBtn">Description</button>
+        </div>
+    `;
+
+    console.log(this.trailer.results[0].key);
+
+    document.getElementById("descriptionBtn").addEventListener("click", () => {
+      this.showDescription();
+    });
+
+    this.addBackListener();
+    this.addDescriptionListener();
   }
 }
 
