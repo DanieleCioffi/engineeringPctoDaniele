e52d783a41c7dbcd58e9bbf13afaf700149022f0
DanieleCioffi <85867609+DanieleCioffi@users.noreply.github.com>
2021-06-28 16:07:47 +0200

    style: tradotti button accedi e registrati

--- src/views/components/navbar/Navbar.js
+++ src/views/components/navbar/Navbar.js
@@ -4,8 +4,8 @@ class Navbar {
             <p align="center">
                 <img src="./src/Assets/logo.png" id="logo" class="rounded" width="50">
                 <a id="title">Cinema</a>
-                <button id="login" class="btn btn-primary">Accedi</button>
-                <button id="register" class="btn btn-secondary">Registrati</button>
+                <button id="login" class="btn btn-primary">Log in</button>
+                <button id="register" class="btn btn-secondary">Sign up</button>
             </p>
         </div>`;
   }
