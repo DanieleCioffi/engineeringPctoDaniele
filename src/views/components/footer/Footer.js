d1b28fd8e4633be0889e8dc3ae3b49e3dc94d6a8
DanieleCioffi <85867609+DanieleCioffi@users.noreply.github.com>
2021-06-30 00:43:04 +0200

    style: cambio layout footer

--- src/views/components/footer/Footer.js
+++ src/views/components/footer/Footer.js
@@ -6,9 +6,11 @@ class Footer {
                 <img class="englogo" src = "./src/Assets/eng.png">
             </div>
             
-            <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-                © 2021 Copyright:
-                <a class="text-dark" href="https://www.eng.it/">Engineering S.p.A.</a>
+            <div class="text-center text-dark p-3" style="background-color: #f1f1f1;">
+                <p class="text-dark" style="margin: 0">
+                    © 2021 Copyright:
+                    <a class="text-dark" href="https://www.eng.it/">Engineering S.p.A.</a>
+                </p>
             </div>
         </footer>`;
   }
