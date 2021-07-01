b63719a95360d35dce076d63363eed1c21c337b7
DanieleCioffi <85867609+DanieleCioffi@users.noreply.github.com>
2021-07-01 12:27:24 +0200

    fix: aggiunto Route.js

---
+++ src/Route.js
@@ -0,0 +1,9 @@
+import Details from "./views/components/details/Details";
+import Home from "./views/components/home/Home";
+
+const Route = new Map([
+  ["homepage", Home],
+  ["details", Details],
+]);
+
+export default Route;
